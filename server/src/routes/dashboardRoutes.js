aed8a024d022e986068c36d82cdb87e715ff4b7d
Yogesh Bhagat <yogeshsbhagat1312@gmail.com>
2024-10-02 12:40:35 +0530

    changes made

--- server/src/routes/dashboardRoutes.js
+++ server/src/routes/dashboardRoutes.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middleware/authMiddleware');
 
-router.get('/MDashboard', authenticateToken, (req, res) => {
+router.get('/mdashboard', authenticateToken, (req, res) => {
   const userId = req.user.userId; // Dynamic user ID
   // Fetch data based on userId
   // ...
   res.json({ message: 'Manager dashboard data', userId });
 });
 
-router.get('/EDashboard', authenticateToken, (req, res) => {
+router.get('/edashboard', authenticateToken, (req, res) => {
   const userId = req.user.userId; // Dynamic user ID
   // Fetch data based on userId
   // ...
